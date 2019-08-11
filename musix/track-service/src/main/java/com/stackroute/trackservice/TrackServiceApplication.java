298d125eee8c7183640bfbdf773deb07bc811e98
naveen prakash <naveenprakashd@gmail.comgit onfig --global user.email naveenprakashd@gmail.com>
2019-08-12 00:19:45 +0530

    committing after resolving all issues for spring boot test cases

--- musix/track-service/src/main/java/com/stackroute/trackservice/TrackServiceApplication.java
+++ musix/track-service/src/main/java/com/stackroute/trackservice/TrackServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@EnableConfigurationProperties(ApplicationListenerSeedData.class)
+//@EnableCconfigurationProperties(ApplicationListenerSeedData.class)
 @EnableSwagger2 //enabling swagger2 to view the api documentation
 @SpringBootApplication//this tells the dispacther servlet that this class is the main class
 public class TrackServiceApplication {
